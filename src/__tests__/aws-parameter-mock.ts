058a0d4074756378c3824073e3597b0027061a3f
John Donovan <john@asktia.com>
2019-12-20 15:25:21 -0800

    Fixes unhandled rejection in Parameter.value

--- src/__tests__/aws-parameter-mock.ts
+++ src/__tests__/aws-parameter-mock.ts
@@ -2,9 +2,13 @@ import { SSM } from 'aws-sdk';
 
 export class AwsParameterMock {
   private parameters: { [key: string]: SSM.GetParameterResult } | undefined;
+  private rejects = false;
+  private rejectionMessage: string | undefined;
 
   public reset(): void {
     this.parameters = undefined;
+    this.rejects = false
+    this.rejectionMessage = undefined
   }
 
   public addParameter(param: SSM.PutParameterRequest): void {
@@ -16,15 +20,23 @@ export class AwsParameterMock {
     };
   }
 
-  public get implementation() {
+  public rejectsPromise(message = 'failed to fetch param'): void {
+    this.rejects = true
+    this.rejectionMessage = message 
+  }
+
+  public get implementation(): Record<string, Function> {
     return {
       getParameter: jest.fn(params => ({
         promise: jest.fn(() => {
           return new Promise((resolve, reject) => {
-            if (this.parameters && this.parameters[params.Name]) {
-              resolve(this.parameters[params.Name]);
+            if(this.rejects) {
+              reject(new Error(this.rejectionMessage))
             }
-            reject();
+            if (this.parameters && this.parameters[params.Name]) {
+              return resolve(this.parameters[params.Name]);
+            }
+            reject(new Error('Missing param'));
           });
         })
       }))
